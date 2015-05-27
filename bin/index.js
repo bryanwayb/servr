851620c93339139a49016e22dd128d77fde43c83
Bryan Way <bryanwayb@gmail.com>
2015-05-27 01:39:37 -0500

    Command line parsing for config files works as does actualy loading those configurations and merging them.

---
+++ bin/index.js
@@ -0,0 +1,58 @@
+var arguments = require('minimist')(process.argv.slice(2)),
+	server = require('../lib/index.js'),
+	library = require('../lib/functions.js');
+
+function resolveConfigAlias(config, entry) {
+	if(typeof entry === 'string') {
+		entry = resolveConfigAlias(config, config[entry]);
+	}
+	return entry;
+}
+
+function loadConfig(path) {
+	var config = require(path);
+	
+	for(var property in config.hosts) {
+		if(config.hasOwnProperty(property)) {
+			config[property] = resolveConfigAlias(config, config[property]);
+		}
+	}
+	
+	return config;
+}
+
+// Try to load the default configuration file. If we're not able to for some reason, continue on with a warning.
+var defaultConfiguration = undefined;
+try {
+	defaultConfiguration = loadConfig('../conf/config.json');
+}
+catch(e) {
+	library.makeWarning('Continuing without loading a default configuration');
+	defaultConfiguration = { };
+}
+
+var externalConfigurations = library.buildFlatArray([ arguments.c, arguments.config ]);
+var config = undefined;
+for(var i = 0; i < externalConfigurations.length; i++) {
+	try {
+		if(config === undefined) {
+			config = loadConfig(externalConfigurations[i]);
+		}
+		else {
+			library.mergeObjects(config, loadConfig(externalConfigurations[i]));
+		}
+	}
+	catch(e) {
+		library.makeError('Unable to load configuration file: \'' + externalConfigurations[i] + '\'', 1);
+	}
+}
+
+if(config !== undefined) {
+	library.mergeObjects(config, defaultConfiguration);
+}
+else {
+	library.makeInfo('Using only default configuraiton');
+	config = defaultConfiguration;
+}
+
+console.log(config);
