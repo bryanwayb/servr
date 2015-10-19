fdb86ac01538b6ba6af15bbb5d58800aa9f44b3f
Bryan Way <bryanwayb@gmail.com>
2015-10-19 11:48:48 -0500

    Added test directory to lint checks

--- test/check.js
+++ test/check.js
@@ -20,19 +20,19 @@ function recursiveCheckDirectory(dir) {
             if(fs.statSync(pathname).isDirectory()) {
                 recursiveCheckDirectory(pathname);
             }
-            else if (path.extname(pathname).toLowerCase() === '.js') {
+            else if(path.extname(pathname).toLowerCase() === '.js') {
                 var fileContents = fs.readFileSync(pathname).toString();
                 var totalErrors = 0;
-                
+
                 jshint(fileContents, require('./jshint.json'));
                 totalErrors += jshint.errors.length;
-                
+
                 var jscsChecker = new JSCS();
                 jscsChecker.registerDefaultRules();
                 jscsChecker.configure(require('./jscs.json'));
                 var jscsErrors = jscsChecker.checkString(fileContents).getErrorList();
                 totalErrors += jscsErrors.length;
-                
+
                 var jscsFixableErrors = 0;
                 for(var o in jscsErrors) {
                     if(jscsErrors.hasOwnProperty(o) && jscsErrors[o].fixed) {
@@ -40,43 +40,41 @@ function recursiveCheckDirectory(dir) {
                         totalFixableErrorsInFiles++;
                     }
                 }
-                
+
                 if(allowFix) {
                     totalErrors -= jscsFixableErrors;
                 }
-                
+
                 var success = totalErrors === 0;
                 totalErrorsInFiles += totalErrors;
-                
+
                 var printColor = success ? colors.green : colors.red;
                 console.log(printColor('[' + (success ? 'PASS' : 'FAIL') + '] ') + pathname);
-                
+
                 totalFiles++;
                 if(!success || jscsFixableErrors > 0) {
                     console.log('  ' + totalErrors + ' error' + (totalErrors === 1 ? '' : 's'));
-                    
+
                     if(jscsFixableErrors > 0) {
                         console.log('  ' + jscsFixableErrors + ' fixable error' + (jscsFixableErrors === 1 ? '' : 's'));
                     }
-                    
-                    for(var o in jshint.errors) {
-                        if(jshint.errors.hasOwnProperty(o)) {
-                            console.log(colors.red('    code  [' + jshint.errors[o].line + ', ' + jshint.errors[o].character + ']: ' + jshint.errors[o].reason));
+
+                    for(var n in jshint.errors) {
+                        if(jshint.errors.hasOwnProperty(n)) {
+                            console.log(colors.red('    code  [' + jshint.errors[n].line + ', ' + jshint.errors[n].character + ']: ' + jshint.errors[n].reason));
                         }
                     }
 
-                    for(var o in jscsErrors) {
-                        if(jscsErrors.hasOwnProperty(o)) {
-                            console.log((jscsErrors[o].fixed && allowFix ? colors.green : colors.yellow)('    style [' + jscsErrors[o].line + ', ' + jscsErrors[o].column + ']: ' + jscsErrors[o].message + (jscsErrors[o].fixed ? ' (fix' + (allowFix ? 'ed' : 'able') + ')' : '')));
+                    for(var p in jscsErrors) {
+                        if(jscsErrors.hasOwnProperty(p)) {
+                            console.log((jscsErrors[p].fixed && allowFix ? colors.green : colors.yellow)('    style [' + jscsErrors[p].line + ', ' + jscsErrors[p].column + ']: ' + jscsErrors[p].message + (jscsErrors[p].fixed ? ' (fix' + (allowFix ? 'ed' : 'able') + ')' : '')));
                         }
                     }
-                    
-                    console.log();
-                    
+
                     if(jscsFixableErrors > 0 && allowFix) {
                         fs.writeFileSync(pathname, jscsChecker.fixString(fileContents).output);
                     }
-                    
+
                     if(!success) {
                         returnCode++;
                     }
@@ -88,6 +86,7 @@ function recursiveCheckDirectory(dir) {
 
 recursiveCheckDirectory(path.resolve(__dirname, '../bin'));
 recursiveCheckDirectory(path.resolve(__dirname, '../lib'));
+recursiveCheckDirectory(path.resolve(__dirname, '../test'));
 
 console.log('\n' + (totalFiles - returnCode) + '/' + totalFiles + ' files passed (' + totalErrorsInFiles + ' error' + (totalErrorsInFiles === 1 ? '' : 's') + ', ' + totalFixableErrorsInFiles + ' fix' + (allowFix ? 'ed' : 'able') + ')');
 
