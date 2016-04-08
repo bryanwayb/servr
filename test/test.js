bfed94f6b7d5a4cc6f915b8cb680222e70fa5058
Bryan Way <bryanwayb@gmail.com>
2016-04-08 02:23:53 -0500

    Basic API testing

--- test/test.js
+++ test/test.js
@@ -4,8 +4,18 @@ var servr = require('../lib/index.js');
 
 module.exports = {
     'API Access': function(test) {
-        test.notEqual(servr.Server, undefined, 'servr.Server function is not defined');
-        
+        test.notEqual(servr.pool, undefined, 'servr.pool function is not defined');
+
+        var instance = servr('*:1337', {
+        });
+        test.notEqual(instance.start, undefined, 'servr.prototype.start function is not defined');
+
+        instance = servr.pool({
+            '*:1337': {
+            }
+        });
+        test.notEqual(instance.start, undefined, 'servr.pool.prototype.start function is not defined');
+
         test.done();
     }
 };
