90f420ba5767afe725d2f34553c04a16cfcd2ba9
Bryan Way <bryanwayb@gmail.com>
2016-04-08 02:11:11 -0500

    Consolidated pool.js and server.js to index.js

--- bin/worker.js
+++ bin/worker.js
@@ -7,6 +7,6 @@ var servr = require('../lib/index.js'),
 // This is the actual server worker. This should only handle server operations, independent of
 // cluster management (which should be handled by the command line entry file, index.js)
 module.exports = function(configuration) {
-    servr.ServerPool(configuration).start();
+    servr.pool(configuration).start();
     logger.log(enums.LogType.Info, 'listening');
 };
