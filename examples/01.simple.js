8ad29a9410237e34a4d3f1be3507577f5b2af1a7
Bryan Way <bryanwayb@gmail.com>
2016-04-08 02:30:44 -0500

    Added basic filesystem site example

--- examples/01.simple.js
+++ examples/01.simple.js
@@ -1,2 +1,21 @@
 'use strict';
 
+/*
+    Simple example showing a filesystem based website
+*/
+
+var servr = require('../lib/index.js');
+
+servr({
+    host: 'localhost',
+    port: 8080
+}, {
+    protocol: 'http',
+    directories: {
+        '/': {
+            root: '../www/',
+            options: '*',
+            default: [ 'index.jshtml', 'index.html', 'default.html' ]
+        }
+    }
+}).start();
